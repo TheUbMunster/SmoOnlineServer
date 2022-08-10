e09e8c012a3c0af4a769713e209f583109fb5851
TheUbMunster <66451362+TheUbMunster@users.noreply.github.com>
2022-08-09 18:10:51 -0600

    Added resources for cross platform project, begin working on the UI

--- ProxChatClientGUICrossPlatform/MainWindow.cs
+++ ProxChatClientGUICrossPlatform/MainWindow.cs
@@ -1,24 +1,48 @@
 using Gtk;
 using System;
 using UI = Gtk.Builder.ObjectAttribute;
+using IOPath = System.IO.Path;
 
 namespace ProxChatClientGUICrossPlatform
 {
     internal class MainWindow : Window
     {
-        [UI] private Label _label1 = null;
-        [UI] private Button _button1 = null;
-
-        private int _counter;
-
+        [UI] private readonly Image settingsGearImage;
+        [UI] private readonly Image connectImage;
+        [UI] private readonly Image disconnectImage;
+        [UI] private readonly Image directImage;
+        [UI] private readonly Image teamImage;
+        [UI] private readonly Image globalImage;
+        [UI] private readonly Image headphonesImage;
+        [UI] private readonly Image headphonesCrossedImage;
+        [UI] private readonly Image micImage;
+        [UI] private readonly Image micCrossedImage;
+        [UI] private Button settingsButton;
         public MainWindow() : this(new Builder("MainWindow.glade")) { }
 
         private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
         {
             builder.Autoconnect(this);
-
             DeleteEvent += Window_DeleteEvent;
-            _button1.Clicked += Button1_Clicked;
+            //populate UI fields
+            settingsButton = (Button)builder.GetObject("settingsButton");
+
+            settingsGearImage = ResizeImage(IOPath.Join("Images", "Gear.png"), 75, 75);
+            settingsGearImage.Show();
+            settingsButton.Add(settingsGearImage);
+
+            connectImage = ResizeImage(IOPath.Join("Images", "connect.png"), 75, 75);
+            connectImage.Show();
+
+            disconnectImage = ResizeImage(IOPath.Join("Images", "disconnect.png"), 75, 75);
+            disconnectImage.Show();
+
+            //settings of UI fields
+            this.SetSizeRequest(700, 400); //set minimum size of main window
+
+            //settingsButton.Clicked += (_,_) => { Application.Quit(); }; //testing
+
+            settingsButton.SetSizeRequest(75, 75);
         }
 
         private void Window_DeleteEvent(object sender, DeleteEventArgs a)
@@ -26,10 +50,13 @@ namespace ProxChatClientGUICrossPlatform
             Application.Quit();
         }
 
-        private void Button1_Clicked(object sender, EventArgs a)
+        #region Utility
+        private static Image ResizeImage(string path, int width, int height)
         {
-            _counter++;
-            _label1.Text = "Hello World! This button has been clicked " + _counter + " time(s).";
+            Gdk.Pixbuf pixbuf = new Gdk.Pixbuf(path);
+            pixbuf = pixbuf.ScaleSimple(width, height, Gdk.InterpType.Bilinear);
+            return new Image(pixbuf);
         }
+        #endregion
     }
 }
