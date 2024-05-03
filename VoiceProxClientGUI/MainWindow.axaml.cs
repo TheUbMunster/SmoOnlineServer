d713a014b977cb7920a43968485b9ecd0abdba1e
TheUbMunster <theubmunster@gmail.com>
2024-05-03 02:27:55 -0600

    added avalonia template

---
+++ VoiceProxClientGUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VoiceProxClientGUI
+{
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+   }
+}
